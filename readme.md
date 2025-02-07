0e18ddb88858d09afaa92c47ec01d4baef7b8dc4
jiagengliu <108316583+jiagengliu02@users.noreply.github.com>
2025-02-06 19:24:23 -0500

    first commit

---
+++ readme.md
@@ -0,0 +1,53 @@
+# <center>SPIRE选课工具
+
+## 前言
+- 本工具仅作为作者朋友自用，严禁外传。
+- 本工具开发并不完整，只是为了省事做的一个小工具。
+- 使用中文是为了防止不必要的麻烦
+- `session`会过期，一次挂几个小时，每次请求隔十秒
+
+## 介绍
+- 原理：伪装你自己直接发POST包给SPIRE服务器提出请求
+- 验证机制：关于SPIRE的验证需要三个东西
+> - - `ICSID`(在POST包的`payload`里，每个`session`保持不变)
+> - - `ICStateNum`(及其简单的验证，初始访问的时候赋予一个整数值，随后每次访问页面+1)
+> - - `Cookie`(验证你的身份)
+
+- 你需要做什么：
+> - - 1.配置好一个极其简单的python环境用于发包
+> - - 2.登入你的`spire`账户，把你想选的课加到购物车里
+> - - 3.按f12进入开发者工具，获取`Payload`里的`ICStateNum`和`Cookie`(后面会有详细截图步骤)
+
+## 使用
+
+
+### 教学视频链接
+
+
+### 配置python环境
+```bash
+pip install -r requirements.txt
+```
+
+### (建议使用firefox浏览器)登录SPIRE选课系统并且到达购物车页面，加入你想选的课程到购物车里，此时课程的状态是Closed。
+![](images/1.png)
+### 在你的终端运行script.py文件
+```bash
+python script.py
+```
+
+### 按下F12，选择上方的Network窗口，勾选你的课程并且点击Enroll按钮，点击Yes
+![](images/2.png)
+### 此时你的Network里应该有两个POST包，选择那个更大的12kb的包
+![](images/3.png)
+### 找到这个包的Header里，Request Header，Cookie，右键点击复制value，粘贴进终端按下回车
+![](images/4.png)
+### 同样，找到这个包的Request里ICSID，右键点击复制value，粘贴进终端按下回车
+![](images/5.png)
+### 如果终端打印Success，说明启动成功
+
+
+## 补充
+- spire的验证方式还是非常原始的，理论上除了登录需要SMS验证非常麻烦以外，其他的手动重复操作都是可以脚本执行的
+- 关于如何做这个脚本，需要用浏览器的开发者模式重复发包，观察消息头和payload里面有多少验证方式，以及如何变换的，中间可能涉及到一些加密方式
+- This Tool is Just for Fun, 如有侵权我会删除repo
---
+++ script.py
@@ -0,0 +1,196 @@
+import requests
+from bs4 import BeautifulSoup
+from time import sleep
+import argparse
+import random
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--sleep_duration", type=int, default=10, help="访问间隔")
+    parser.add_argument("--cookie", type=str, help="从网页中复制")
+    parser.add_argument("--icsid", type=str, help="从网页中复制")
+    
+    # 解析参数
+    args = parser.parse_args()
+    ############# GET Current ICStateNum #############
+    http_request = {
+        "method": "GET",
+        "url": "https://www.spire.umass.edu/psc/heproda_26/EMPLOYEE/SA/c/SSR_STUDENT_FL.SSR_SHOP_CART_FL.GBL",
+        "params": {
+            "Action": "U",
+            "MD": "Y",
+            "GMenu": "SSR_STUDENT_FL",
+            "GComp": "SSR_START_PAGE_FL",
+            "GPage": "SSR_START_PAGE_FL",
+            "scname": "CS_SSR_MANAGE_CLASSES_NAV"
+        },
+        "headers": {
+            "Host": "www.spire.umass.edu",
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:135.0) Gecko/20100101 Firefox/135.0",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+            "Accept-Language": "en-US,en;q=0.5",
+            "Accept-Encoding": "gzip, deflate, br, zstd",
+            "Referer": "https://login.microsoftonline.com/",
+            "Connection": "keep-alive",
+            "Cookie": args.cookie,
+            "Upgrade-Insecure-Requests": "1",
+            "Sec-Fetch-Dest": "document",
+            "Sec-Fetch-Mode": "navigate",
+            "Sec-Fetch-Site": "cross-site",
+            "Sec-Fetch-User": "?1",
+            "Priority": "u=0, i"
+        }
+    }
+    response = requests.get(http_request['url'], headers=http_request['headers'])
+    soup = BeautifulSoup(response.text, "html.parser")
+    ic_state_num_tag = soup.find("input", {"name": "ICStateNum"})
+    if ic_state_num_tag:
+        ic_state_num = ic_state_num_tag.get("value")
+        print("ICStateNum:", ic_state_num)
+    else:
+        print("未找到 ICStateNum")
+
+
+    ############## POST Request On your shop cart ############
+    http_request_post_1 = {
+        "method": "POST",
+        "url": "https://www.spire.umass.edu/psc/heproda_26/EMPLOYEE/SA/c/SSR_STUDENT_FL.SSR_SHOP_CART_FL.GBL",
+        "headers": {
+            "Host": "www.spire.umass.edu",
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:135.0) Gecko/20100101 Firefox/135.0",
+            "Accept": "*/*",
+            "Accept-Language": "en-US,en;q=0.5",
+            "Accept-Encoding": "gzip, deflate, br, zstd",
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Origin": "https://www.spire.umass.edu",
+            "Connection": "keep-alive",
+            "Referer": "https://www.spire.umass.edu/psc/heproda_26/EMPLOYEE/SA/c/SSR_STUDENT_FL.SSR_MD_SP_FL.GBL"
+                    "?Action=U&MD=Y&GMenu=SSR_STUDENT_FL&GComp=SSR_START_PAGE_FL&"
+                    "GPage=SSR_START_PAGE_FL&scname=CS_SSR_MANAGE_CLASSES_NAV",
+            "Cookie":  args.cookie,
+            "Sec-Fetch-Dest": "empty",
+            "Sec-Fetch-Mode": "cors",
+            "Sec-Fetch-Site": "same-origin"
+        },
+        "data":{
+            "ICAJAX": 1,
+            "ICNAVTYPEDROPDOWN": 0,
+            "ICType": "Panel",
+            "ICElementNum": 26,
+            "ICStateNum": 132,
+            "ICAction": "DERIVED_SSR_FL_SSR_ENROLL_FL",
+            "ICModelCancel": 0,
+            "ICXPos": 0,
+            "ICYPos": 0,
+            "ResponsetoDiffFrame": -1,
+            "TargetFrameName": None,
+            "FacetPath": None,
+            "ICFocus": None,
+            "ICSaveWarningFilter": 0,
+            "ICChanged": 0,
+            "ICSkipPending": 0,
+            "ICAutoSave": 0,
+            "ICResubmit": 0,
+            "ICSID":  args.icsid,
+            "ICActionPrompt": False,
+            "ICBcDomData": "C~UnknownValue~EMPLOYEE~SA~SSR_STUDENT_FL.SSR_MD_SP_FL.GBL~..."
+                        "UnknownValue*C~UnknownValue~EMPLOYEE~SA~SSR_STUDENT_FL.SSR_MD_SP_FL.GBL~..."
+                        "UnknownValue*C~UnknownValue~EMPLOYEE~SA~SSR_STUDENT_FL.SSR_MD_SP_FL.GBL~...",
+            "ICDNDSrc": None,
+            "ICPanelHelpUrl": None,
+            "ICPanelName": None,
+            "ICPanelControlStyle": "pst_side1-fixed pst_panel-mode pst_side2-disabled pst_side2-hidden",
+            "ICFind": None,
+            "ICAddCount": None,
+            "ICAppClsData": None,
+            "win26hdrdivPT_SYSACT_PRVLST": None,
+            "win26hdrdivPT_SYSACT_NXTLST": None,
+            "win26hdrdivPT_SYSACT_RETLST": "psc_hidden",
+            "win26hdrdivPT_SYSACT_HELP": "psc_hidden",
+            "DERIVED_REGFRM1_SSR_SELECT$chk$0": "Y",
+            "DERIVED_REGFRM1_SSR_SELECT$0": "Y"
+        }
+    }
+
+    sleep(1)
+    http_request_post_1['data']['ICStateNum'] = str(int(ic_state_num))
+    response = requests.post(http_request_post_1['url'], headers=http_request_post_1['headers'], data=http_request_post_1['data'])
+
+    if "Are you sure you want to enroll" in response.text:
+        print("Success!!!!!")
+    else:
+        # print(response.text)
+        print("Error!!!!!")
+
+    http_request_post_2 = {
+        "method": "POST",
+        "url": "https://www.spire.umass.edu/psc/heproda_26/EMPLOYEE/SA/c/SSR_STUDENT_FL.SSR_SHOP_CART_FL.GBL",
+        "headers": {
+            "Host": "www.spire.umass.edu",
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:135.0) Gecko/20100101 Firefox/135.0",
+            "Accept": "*/*",
+            "Accept-Language": "en-US,en;q=0.5",
+            "Accept-Encoding": "gzip, deflate, br, zstd",
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Origin": "https://www.spire.umass.edu",
+            "Connection": "keep-alive",
+            "Referer": "https://www.spire.umass.edu/psc/heproda_26/EMPLOYEE/SA/c/SSR_STUDENT_FL.SSR_MD_SP_FL.GBL?Action=U&MD=Y&GMenu=SSR_STUDENT_FL&GComp=SSR_START_PAGE_FL&GPage=SSR_START_PAGE_FL&scname=CS_SSR_MANAGE_CLASSES_NAV",
+            "Cookie":  args.cookie,
+            "Sec-Fetch-Dest": "empty",
+            "Sec-Fetch-Mode": "cors",
+            "Sec-Fetch-Site": "same-origin",
+            "Priority": "u=0"
+        },
+        "data":{
+            "ICAJAX": "1",
+            "ICNAVTYPEDROPDOWN": "0",
+            "ICType": "Panel",
+            "ICElementNum": "26",
+            "ICStateNum": "103",
+            "ICAction": "#ICYes",
+            "ICModelCancel": "0",
+            "ICXPos": "0",
+            "ICYPos": "0",
+            "ResponsetoDiffFrame": "-1",
+            "TargetFrameName": "None",
+            "FacetPath": "None",
+            "ICFocus": "",
+            "ICSaveWarningFilter": "0",
+            "ICChanged": "0",
+            "ICSkipPending": "0",
+            "ICAutoSave": "0",
+            "ICResubmit": "0",
+            "ICSID":  args.icsid,
+            "ICActionPrompt": "false",
+            "ICBcDomData": "C~UnknownValue~EMPLOYEE~SA~SSR_STUDENT_FL.SSR_MD_SP_FL.GBL~SSR_MD_TGT_PAGE_FL~Manage Classes~UnknownValue~UnknownValue~https://www.spire.umass.edu/psc/heproda_26/EMPLOYEE/SA/c/SSR_STUDENT_FL.SSR_MD_SP_FL.GBL?Action=U&MD=Y&GMenu=SSR_STUDENT_FL&GComp=SSR_START_PAGE_FL&GPage=SSR_START_PAGE_FL&scname=CS_SSR_MANAGE_CLASSES_NAV~UnknownValue*C~UnknownValue~EMPLOYEE~SA~SSR_STUDENT_FL.SSR_MD_SP_FL.GBL~SSR_MD_TGT_PAGE_FL~Manage Classes~UnknownValue~UnknownValue~https://www.spire.umass.edu/psc/heproda_26/EMPLOYEE/SA/c/SSR_STUDENT_FL.SSR_MD_SP_FL.GBL?Action=U&MD=Y&GMenu=SSR_STUDENT_FL&GComp=SSR_START_PAGE_FL&GPage=SSR_START_PAGE_FL&scname=CS_SSR_MANAGE_CLASSES_NAV~UnknownValue*C~UnknownValue~EMPLOYEE~SA~SSR_STUDENT_FL.SSR_MD_SP_FL.GBL~SSR_MD_TGT_PAGE_FL~Manage Classes~UnknownValue~UnknownValue~https://www.spire.umass.edu/psc/heproda_26/EMPLOYEE/SA/c/SSR_STUDENT_FL.SSR_MD_SP_FL.GBL?Action=U&MD=Y&GMenu=SSR_STUDENT_FL&GComp=SSR_START_PAGE_FL&GPage=SSR_START_PAGE_FL&scname=CS_SSR_MANAGE_CLASSES_NAV~UnknownValue*C~UnknownValue~EMPLOYEE~SA~SSR_STUDENT_FL.SSR_MD_SP_FL.GBL~SSR_MD_TGT_PAGE_FL~Manage Classes~UnknownValue~UnknownValue~https://www.spire.umass.edu/psc/heproda_26/EMPLOYEE/SA/c/SSR_STUDENT_FL.SSR_MD_SP_FL.GBL?Action=U&MD=Y&GMenu=SSR_STUDENT_FL&GComp=SSR_START_PAGE_FL&GPage=SSR_START_PAGE_FL&scname=CS_SSR_MANAGE_CLASSES_NAV~UnknownValue*C~UnknownValue~EMPLOYEE~SA~SSR_STUDENT_FL.SSR_MD_SP_FL.GBL~SSR_MD_TGT_PAGE_FL~Manage Classes~UnknownValue~UnknownValue~https://www.spire.umass.edu/psc/heproda_26/EMPLOYEE/SA/c/SSR_STUDENT_FL.SSR_MD_SP_FL.GBL?Action=U&MD=Y&GMenu=SSR_STUDENT_FL&GComp=SSR_START_PAGE_FL&GPage=SSR_START_PAGE_FL&scname=CS_SSR_MANAGE_CLASSES_NAV~UnknownValue*C~UnknownValue~EMPLOYEE~SA~SSR_STUDENT_FL.SSR_MD_SP_FL.GBL~SSR_MD_TGT_PAGE_FL~Manage Classes~UnknownValue~UnknownValue~https://www.spire.umass.edu/psc/heproda_26/EMPLOYEE/SA/c/SSR_STUDENT_FL.SSR_MD_SP_FL.GBL?Action=U&MD=Y&GMenu=SSR_STUDENT_FL&GComp=SSR_START_PAGE_FL&GPage=SSR_START_PAGE_FL&scname=CS_SSR_MANAGE_CLASSES_NAV~UnknownValue*C~UnknownValue~EMPLOYEE~SA~SSR_STUDENT_FL.SSR_MD_SP_FL.GBL~SSR_MD_TGT_PAGE_FL~Manage Classes~UnknownValue~UnknownValue~https://www.spire.umass.edu/psc/heproda_26/EMPLOYEE/SA/c/SSR_STUDENT_FL.SSR_MD_SP_FL.GBL?Action=U&MD=Y&GMenu=SSR_STUDENT_FL&GComp=SSR_START_PAGE_FL&GPage=SSR_START_PAGE_FL&scname=CS_SSR_MANAGE_CLASSES_NAV~UnknownValue",
+            "ICDNDSrc": "",
+            "ICPanelHelpUrl": "",
+            "ICPanelName": "",
+            "ICPanelControlStyle": "pst_side1-fixed pst_panel-mode  pst_side2-disabled pst_side2-hidden",
+            "ICFind": "",
+            "ICAddCount": "",
+            "ICAppClsData": "",
+            "win26hdrdivPT_SYSACT_PRVLST": "",
+            "win26hdrdivPT_SYSACT_NXTLST": ""
+        }
+    }
+
+    bias = 0
+    while True:
+        bias += 1
+        sleep(args.sleep_duration+ random.gauss(0, 1))
+        http_request_post_2['data']['ICStateNum'] = str(int(ic_state_num)+bias)
+        response = requests.post(http_request_post_2['url'], headers=http_request_post_2['headers'], data=http_request_post_2['data'])
+
+        if "This page is no longer available" in response.text:
+            print("Error, Session Expired!!!!!")
+        elif "full" in response.text:
+            print("Success!!!!!")
+        else:
+            print("Error, Please Restart!!!!")
+
+if __name__ == "__main__":
+    main()
+
+
+
